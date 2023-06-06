3b3203e365d7742525a7a00421f566fb5e6a3bf6
Arun Gopi <arungopi@live.com>
2023-06-06 19:08:11 +0530

    updated the move function

--- spotify_transformation_load.py
+++ spotify_transformation_load.py
@@ -1,36 +1,105 @@
 import json
-import os
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
 import boto3
 from datetime import datetime
+import pandas as pd
+from io import StringIO
+
+def album(data):
+    album_list=[]
+    for row in data['items']:
+        album_id=row['track']['album']['id']
+        album_name=row['track']['album']['name']
+        album_release_date=row['track']['album']['release_date']
+        album_total_tracks=row['track']['album']['total_tracks']
+        album_url=row['track']['album']['external_urls']['spotify']
+        album_element = {'album_id':album_id,'name':album_name,'release_date':album_release_date,'total_tracks':album_total_tracks,'url':album_url}
+        album_list.append(album_element)
+    return album_list
+
+def artist(data):
+    artist_list=[]
+    for row in data['items']:
+        for key,value in row.items():
+            if key == 'track':
+                for artist in value['artists']:
+                    artist_dic={'artist_id':artist['id'],'artist_name':artist['name'],'external_url':artist['href']}
+                    artist_list.append(artist_dic)
+    return artist_list
+
+def songs(data):
+    song_list=[]
+    for row in data['items']:
+        song_id=row['track']['id']
+        song_name=row['track']['name']
+        song_duration_ms=row['track']['duration_ms']
+        song_url=row['track']['external_urls']['spotify']
+        song_popularity=row['track']['popularity']
+        song_added=row['added_at']
+        album_id=row['track']['album']['id']
+        artist_id=row['track']['artists'][0]['id']
+        song_element={'song_id':song_id,'song_name':song_name,'duration_ms':song_duration_ms,'url':song_url,
+                      'popularity':song_popularity,'song_added':song_added,'album_id':album_id,'artist_id':artist_id}
+        song_list.append(song_element)
+    return song_list
 
 def lambda_handler(event, context):
-    # pull the client id & secret from the env variable
-    client_id = os.environ.get('client_id')
-    client_secret = os.environ.get('client_secret')
+    s3 = boto3.client('s3')
+    Bucket="spotify-etl-project-arungopi"
+    Key="raw_data/to_processed/"
+    files=s3.list_objects(Bucket=Bucket,Prefix=Key)['Contents']
     
-    #authenticating the spotipy client
-    auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-    sp = spotipy.Spotify(auth_manager=auth_manager)
+    spotify_data = []
+    spotify_keys = []
+    for file in files:
+        file_key = file['Key']
+        if file_key.split('.')[-1]=='json':
+            response = s3.get_object(Bucket = Bucket, Key = file_key)
+            content = response['Body']
+            jsonObject = json.loads(content.read())
+            spotify_data.append(jsonObject)
+            spotify_keys.append(file_key)
     
-    #playlist link
-    playlist_link ='https://open.spotify.com/playlist/37i9dQZF1DXcBWIGoYBM5M'
-    playlist_uri=playlist_link.split('/')[-1]
-    
-    #extract data from spotify api
-    spotify_data=sp.playlist_tracks(playlist_uri)
-    
-    #connect s3 client
-    cilent = boto3.client('s3')
-    
-    #generate filename using the datetime.now() function
-    date_string = f'{datetime.now():%Y%m%d%H%M%S%z}'
-    filename = "spotify_raw_" + str(date_string) + ".json"
-    
-    #dump the extracted data in s3 bucket
-    cilent.put_object(
-        Bucket="spotify-etl-project-arungopi",
-        Key="raw_data/to_processed/" + filename,
-        Body=json.dumps(spotify_data)
-        )
+    for data in spotify_data:
+        album_list =album(data)
+        artist_list = artist(data)
+        song_list = songs(data)
+        
+        album_df = pd.DataFrame.from_dict(album_list)
+        album_df=album_df.drop_duplicates(subset=['album_id'])
+        
+        artist_df =pd.DataFrame.from_dict(artist_list)
+        artist_df=artist_df.drop_duplicates(subset=['artist_id'])
+        
+        song_df =pd.DataFrame.from_dict(song_list)
+        
+        album_df['release_date']=pd.to_datetime(album_df['release_date'])
+        song_df['song_added']=pd.to_datetime(song_df['song_added'])
+        
+        date_string = f'{datetime.now():%Y%m%d%H%M%S%z}'
+        
+        songs_key = "transformed_data/songs_data/songs_transformed_" + date_string + ".csv"
+        song_buffer=StringIO()
+        song_df.to_csv(song_buffer, index=False)
+        song_content = song_buffer.getvalue()
+        s3.put_object(Bucket=Bucket, Key=songs_key, Body=song_content)
+        
+        album_key = "transformed_data/album_data/albums_transformed_" + date_string + ".csv"
+        album_buffer =StringIO()
+        album_df.to_csv(album_buffer,index=False)
+        album_content = album_buffer.getvalue()
+        s3.put_object(Bucket=Bucket,Key=album_key,Body=album_content)
+        
+        artist_key = "transformed_data/artist_data/artists_transformed_" + date_string + ".csv"
+        artist_buffer = StringIO()
+        artist_df.to_csv(artist_buffer,index=False)
+        artist_content = artist_buffer.getvalue()
+        s3.put_object(Bucket=Bucket,Key=artist_key,Body=artist_content)
+        
+    s3_resource = boto3.resource('s3')
+    for key in spotify_keys:
+        copy_source = {
+            'Bucket': Bucket,
+            'Key': key
+        }
+        s3_resource.meta.client.copy(copy_source, Bucket, 'raw_data/processed/' + key.split("/")[-1])    
+        s3_resource.Object(Bucket, key).delete()
